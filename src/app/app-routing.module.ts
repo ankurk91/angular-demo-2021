8bec469f6a9bd245d3c9cab19c3034c7bb8f50fb
ankurk91 <pro.ankurk1@gmail.com>
2021-02-28 19:06:11 +0530

    Add forgot password screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from "./Auth/login/login.component";
 import {RegisterComponent} from "./Auth/register/register.component";
+import {ForgotPasswordComponent} from "./Auth/forgot-password/forgot-password.component";
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'forgot-password', component: ForgotPasswordComponent},
   {path: '**', redirectTo: '/'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {LoginComponent} from './Auth/login/login.component';
-import { RegisterComponent } from './Auth/register/register.component'
+import { RegisterComponent } from './Auth/register/register.component';
+import { ForgotPasswordComponent } from './Auth/forgot-password/forgot-password.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
